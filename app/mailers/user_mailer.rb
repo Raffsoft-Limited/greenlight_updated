fe93fc814d581ed00863523abd049660bf8497ce
Fredrick Wampamba <fredrickwampamba1@gmail.com>
2024-02-13 16:45:06 +0300

    other emails to append

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -25,8 +25,8 @@ class UserMailer < ApplicationMailer
   def notify_signup(user_email)
     @user_email = user_email
     # @activation_type = activation_type
-    # mail(to: ['bmpeirwe@gmail.com', 'gndungu@raffsoft.co.ug', 'fwampamba@raffsoft.co.ug', 'iagaba@raffsoft.co.ug'], subject: "New user signed up on Colabo")
-    mail(to: ['fwampamba@raffsoft.co.ug'], subject: "New user signed up on Colabo")
+    mail(to: ['fwampamba@raffsoft.co.ug', 'bmpeirwe@gmail.com', 'gndungu@raffsoft.co.ug', 'iagaba@raffsoft.co.ug'], subject: "New user signed up on Colabo")
+    # mail(to: ['fwampamba@raffsoft.co.ug'], subject: "New user signed up on Colabo")
   end
 
   def verify_email(user, url, settings)
